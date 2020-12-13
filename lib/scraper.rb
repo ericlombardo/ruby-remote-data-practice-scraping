6c3735221ea472bde370a6a610cb09012f8436d4
Eric <eric.m.lombardo@gmail.com>
2020-12-13 16:29:29 -0500

    brought in HTML from Flatiron and iterated through it

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,16 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(open("https://flatironschool.com/")) # brings in HTML from flatiron
+
+course_titles = doc.css(".title-oE5vT4")  # links section to course_titles
+course_titles.each do |course|  # loops through each one and puts the text in each
+  puts course.text
+end
+
+# testing how to find different information about HTML elements
+# puts course_titles[0].attributes
+
+
+
